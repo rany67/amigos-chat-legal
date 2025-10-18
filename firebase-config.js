a279640dd56a04b87dc8c1cb04ac33028ef271fe
rany67 <ranywsomindonesia@gmail.com>
2025-10-18 18:43:00 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,17 @@
-// CONFIGURAÇÃO DO FIREBASE - SUBSTITUA COM SEUS DADOS!
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBpHrKLeNQ18UjMCUJCTMol5JoUBoOMvlg",
-    authDomain: "meu-chat-grupo.firebaseapp.com",
-    projectId: "meu-chat-grupo",
-    storageBucket: "meu-chat-grupo.firebasestorage.app",
-    messagingSenderId: "553703801847",
-    appId: "1:553703801847:web:c8a7d5763de6f1dc1664f2"
+  apiKey: "AIzaSyBpHrKLeNQ18UjMCUJCTMol5JoUBoOMvlg",
+  authDomain: "meu-chat-grupo.firebaseapp.com",
+  projectId: "meu-chat-grupo",
+  storageBucket: "meu-chat-grupo.firebasestorage.app",
+  messagingSenderId: "553703801847",
+  appId: "1:553703801847:web:c8a7d5763de6f1dc1664f2"
 };
 
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
